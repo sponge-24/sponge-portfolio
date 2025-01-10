2b351d7ce97fd9df830aa5b638b64172795c12d3
sudharsan <121513706+sponge-24@users.noreply.github.com>
2025-01-11 01:13:27 +0530

    resolve image 404 isssue

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'am Sudharsanan K</h1>
             <p className={styles.description}>I focus on fixing flaws in low-level systems and building secure, efficient designs with precision and security.</p>
-            <a  href="/resume.pdf" download='resume.pdf' className={styles.contactBtn}>Get My Resume</a>
+            <a  href={import.meta.env.BASE_URL+ "/resume.pdf"} download='resume.pdf' className={styles.contactBtn}>Get My Resume</a>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt='Hero image of me' className={styles.heroImg}/>
     </section>
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const getImageUrl = (path) => {
-    return new URL(`/assets/${path}`, window.location.origin).href;
+    return import.meta.env.BASE_URL + `/assets/${path}`
   };
